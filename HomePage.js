ac401ba046f957e26b58f6fa9677018277ebfe2a
GianlucaGrammatica <147130877+GianlucaGrammatica@users.noreply.github.com>
2024-02-22 13:28:06 +0100

    Add files via upload

--- HomePage.js
+++ HomePage.js
@@ -2,6 +2,8 @@ let SplashCon = document.getElementById("SplashCon");
 let MainUICOn = document.getElementById("MainUICOn");
 let SettingsCon = document.getElementById("SettingsCon");
 let LevelLayer = document.getElementById("LevelLayer");
+let NameLayer = document.getElementById("NameLayer");
+let NoNameLayer = document.getElementById("NoNameLayer");
 
 let MainMenuMusic = document.getElementById("MainMenuMusic");
 let ButtonClickSFX1 = document.getElementById("ButtonClickSFX1");
@@ -10,7 +12,12 @@ let ButtonClickSFX2 = document.getElementById("ButtonClickSFX2");
 let PlayButton = document.getElementById("PlayButton");
 let SettingsButton = document.getElementById("SettingsButton");
 let LevelButonCancel = document.getElementById("LevelButonCancel");
+let NameButonCancel = document.getElementById("NameButonCancel");
+let NameGoButton = document.getElementById("NameGoButton");
+let NameInput = document.getElementById("NameInput");
 let LevelButonNormale = document.getElementById("LevelButonNormale");
+let LevelButonMedio = document.getElementById("LevelButonMedio");
+let LevelButonDifficile = document.getElementById("LevelButonDifficile");
 
 let SettingsMusicCheckbox = document.getElementById("SettingsMusicCheckbox");
 let SettingsSFXCheckbox = document.getElementById("SettingsSFXCheckbox");
@@ -23,32 +30,47 @@ let TapSFX2Items = document.getElementsByClassName("TapSFX2");
 let ShwoingSplash = true;
 let ShowingMain = false;
 let ShowingSettings = false;
+let SelectedLevel = 0
+let LevelsArray = ["index.html", "index.html", "index.html"]
 
 
 /*Storage*/
 let SettingsSaves = {
-    "MusicVolume" : 1,
-    "SFXVolume" : 0.2
+    "MusicVolume": 1,
+    "SFXVolume": 0.2,
+    "Classifica": {
+        "Sandrina" : 182,
+        "Gigiotto" : 12,
+        "Saluun" : 192,
+        "Wabakii" : 182,
+        "Sacrso" : 2,
+        "Otto" : 88
+    },
+    "CurrentPlayer": ""
 }
 
 if (localStorage.getItem('EcoRushSettings')) {
     let TempLoad = localStorage.getItem('EcoRushSettings');
     SettingsSaves = JSON.parse(TempLoad)
-    if(SettingsSaves.MusicVolume == 1){
+    if (SettingsSaves.MusicVolume == 1) {
         SettingsMusicCheckbox.src = "Assets/CheckboxOn.png"
     }
-    else{
+    else {
         SettingsMusicCheckbox.src = "Assets/CheckboxOff.png"
     }
 
-    if(SettingsSaves.SFXVolume == 0.2){
+    if (SettingsSaves.SFXVolume == 0.2) {
         SettingsSFXCheckbox.src = "Assets/CheckboxOn.png"
     }
-    else{
+    else {
         SettingsSFXCheckbox.src = "Assets/CheckboxOff.png"
+    }
+    if(SettingsSaves.CurrentPlayer != undefined && SettingsSaves.CurrentPlayer != 0 && SettingsSaves.CurrentPlayer != ""){
+        NameInput.value = SettingsSaves.CurrentPlayer
     }
 
 }
+
 
 
 /* Tap Sound */
@@ -76,13 +98,13 @@ for (let i = 0; i < TapSFX2Items.length; i++) {
 
 
 /* Other */
-function GoTOPageBlackOverlay(Page){
+function GoTOPageBlackOverlay(Page) {
     BlackOverlay.style.opacity = '1'; // Fading to black
 
-        setTimeout(function () {
-            // Change the page after the transition
-            window.location.href = Page;
-        }, 500);
+    setTimeout(function () {
+        // Change the page after the transition
+        window.location.href = Page;
+    }, 500);
 }
 
 /* Splash Screen */
@@ -170,6 +192,19 @@ SettingsSFXCheckbox.addEventListener("click", function () {
 })
 
 
+/* Name Selector */
+
+function ShowNameLayer() {
+    setTimeout(function () {
+        LevelLayer.classList.add('Hidden');
+        NameLayer.classList.remove('FadeOut');
+        NameLayer.classList.add('FadeIn');
+    }, 420);
+    LevelLayer.classList.add('FadeOut')
+    NameLayer.classList.remove('Hidden');
+    PlayTapSFX1();
+}
+
 /* Level Selection */
 
 LevelButonCancel.addEventListener("click", function () {
@@ -183,6 +218,55 @@ LevelButonCancel.addEventListener("click", function () {
     PlayTapSFX2();
 })
 
-LevelButonNormale.addEventListener("click", function(){
-    GoTOPageBlackOverlay("index.html")
+LevelButonNormale.addEventListener("click", function () {
+    SelectedLevel = 0
+    ShowNameLayer()
+})
+
+LevelButonMedio.addEventListener("click", function () {
+    SelectedLevel = 1
+    ShowNameLayer()
+})
+
+LevelButonDifficile.addEventListener("click", function () {
+    SelectedLevel = 2
+    ShowNameLayer()
+})
+
+
+/* Name Layer */
+
+NameButonCancel.addEventListener("click", function () {
+    setTimeout(function () {
+        NameLayer.classList.add('Hidden');
+        MainUICOn.classList.remove('FadeOut');
+        MainUICOn.classList.add('FadeIn');
+    }, 420);
+    NameLayer.classList.add('FadeOut')
+    MainUICOn.classList.remove('Hidden');
+    PlayTapSFX2();
+})
+
+NameGoButton.addEventListener("click", function () {
+    if (NameInput.value != "") {
+        SettingsSaves.CurrentPlayer = NameInput.value
+        localStorage.setItem('EcoRushSettings', JSON.stringify(SettingsSaves));
+        GoTOPageBlackOverlay(LevelsArray[SelectedLevel])
+        PlayTapSFX1();
+    }
+    else {
+        setTimeout(function () {
+            NoNameLayer.classList.add('FadeOut');
+        }, 1500);
+        setTimeout(function () {
+            NoNameLayer.classList.add('Hidden');
+        }, 1920);
+
+        setTimeout(function () {
+            NoNameLayer.classList.remove('FadeOut');
+            NoNameLayer.classList.add('FadeIn');
+        }, 120);
+        NoNameLayer.classList.remove('Hidden');
+        PlayTapSFX2();
+    }
 })
